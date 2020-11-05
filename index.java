79e0de004e2809e25f2ba51d3f836cc7fc802004
MaxBromley1 <mjbro2003@gmail.com>
2020-11-05 02:16:04 +0000

    I did multiple rollovers

--- index.java
+++ index.java
@@ -7,12 +7,37 @@ void draw() {
 	stroke(0);
 
   //start creating the lines
-
-
+line(10, 10,250,10);
+line(10,200,250,200);
+line(10,10,10,200);
+line(250,10,250,200);
+ line(10,100,250,100);
+  line(125,10,125,200);
   //Fill a black color
-  
+
+	if(mouseX < 125 && mouseY < 100){
+rect(10,10,115,90);
+  fill(0);
+
+}
+	if(mouseX < 125 && mouseY > 100){
+rect(10,100,115,100);
+  fill(0);
+  }
+	if(mouseX > 125 && mouseY < 100){
+rect(125,10,125,90);
+  fill(0);
+  }
+	if(mouseX > 125 && mouseY > 100){
+rect(125,100,125,100);
+  fill(0);
+  }
+
+
+}
+
+
+
 
   //Build your conditional statement 
 	
-
-}
